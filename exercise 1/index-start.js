f80b6faca320d9073b30fa367ac4b8a4c4cb2514
Luis Rogelio Hernández López <kurtz@Kurtz-MacBook-Pro.local>
2018-11-20 15:15:42 -0600

    Write exercise 1

---
+++ exercise 1/index-start.js
@@ -0,0 +1,31 @@
+const people = [
+  {
+    name: "Luis",
+    age: 25
+  },
+  {
+    name: "Osuna",
+    age: 29
+  },
+  {
+    name: "Diego",
+    age: 17
+  },
+  {
+    name: "Tulio",
+    age: 31
+  },
+  {
+    name: "William",
+    age: 31
+  }
+];
+
+function getPeopleUnderAge(people) {
+
+}
+
+// This array should print only the names of the people under 18 years old, as an array of strings.
+console.log(getPeopleUnderAge(peepz));
+// This array should print the same data that it had before.
+console.table(peepz);
---
+++ exercise 1/readme.md
@@ -0,0 +1,8 @@
+# People
+
+## Instructions
+Write a function that returns an array of people that are under 18 years old.
+
+### Acceptance criteria
+- The function must not alter the original array...
+- The function must return an array of strings, containing only the names of the people under 18 years old.
